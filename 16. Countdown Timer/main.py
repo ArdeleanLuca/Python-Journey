0b80799ca5984bebe9f021229a89a8bcb7712d0d
Ardelean Luca <ardeleanluca610@gmail.com>
2026-02-20 16:09:08 +0200

    Add countdown timer functionality in main.py

---
+++ 16. Countdown Timer/main.py
@@ -0,0 +1,10 @@
+import time
+number = int(input("Number: "))
+for x in range(number, 0, -1):
+    seconds = x % 60
+    minutes = int((x / 60) % 60)
+    hours = int((x / 3600) % 24)
+    days = int((x / 86400) % 30)
+    time.sleep(1)
+
+    print(f"\r{days:02}:{hours:02}:{minutes:02}:{seconds:02}", end="")
